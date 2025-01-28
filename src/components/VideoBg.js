a118af555ea7c7677301c137d7e35d8afe9a871a
sagar <sagarmairrajput@gmail.com>
2025-01-28 19:09:29 +0530

    Used the trailer info to render video trailer in bg

--- src/components/VideoBg.js
+++ src/components/VideoBg.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { MOVIE_TRAILER_BY_ID, TMDB_HEADERS } from "../utils/constants"
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addMovieTrailer } from '../utils/moviesSlice';
 
 const VideoBg = ({ movieId }) => {
   const dispatch = useDispatch();
+  const trailerInfo = useSelector((store) => store.moviesList?.movieTrailer);
   const getTrailerById = async (movie_id) => {
     const apiUrl = MOVIE_TRAILER_BY_ID.replace('movie_id', movie_id);
     const resObj = await fetch(apiUrl, TMDB_HEADERS);
@@ -15,13 +16,14 @@ const VideoBg = ({ movieId }) => {
   }
   useEffect(() => {
     getTrailerById(movieId);
+    // console.log(trailerInfo[0].key, "key of trailer");
   }, [])
   return (
     <div>
       <iframe
         width="560"
         height="315"
-        src={"https://www.youtube.com/embed/-ZAohhw3zoY?si="}
+        src={"https://www.youtube.com/embed/" + trailerInfo[0].key}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"
       ></iframe>
